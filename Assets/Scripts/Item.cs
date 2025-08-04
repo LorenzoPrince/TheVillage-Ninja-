e5e30d4e1da99cd3131aec1a75c678d189bfeadb
LorenzoPrince <lorenzoprince222@gmail.com>
2025-08-04 00:21:44 -0300

    Implementa clases iniciales para Player e Items
    Scripts con algunos métodos vacíos. Se completaran mas adelante.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private string _nombre;
+    private string _tipo;
+    private int _valor;
+
+    public string Nombre
+    {
+        get { return _nombre; } // dos formas de aplicar get y getter esta forma un poco mas larga
+        set { _nombre = value; }
+    }
+
+    public string Tipo
+    {
+        get => _tipo;  //esta forma mas corta.
+        set => _tipo = value;
+    }
+
+    public int Valor
+    {
+        get => _valor; // en vez de corchete agrego la flecha. 
+        set
+        {
+            if (value >= 0)
+                _valor = value;
+        }
+    }
+
+
+    public virtual void Usar(Player jugador)
+    {
+
+    }
+
+    public virtual void Collect(Player jugador)
+    {
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,101 @@
 using UnityEngine;
-
-public class Player : MonoBehaviour
+using System.Collections.Generic; //para las listas
+public class Player : MonoBehaviour, IDamageable // implemento interface para asegurar que tenga el metodo recibir daño
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int _vida = 100; // el guion bajo se usa para decir que la variable es interna a la clase, y no se debe cambiar desde fuera.
+    private float _velocidad = 5f;
+    private int _totalMonedas = 0;
+    private Vector2 _posicionActual;
+    private bool _vivo = true;
+    public int Vida
     {
-        
+        get => _vida;  // aplicamos getters y setters para proteger la integridad de las clases y que se modifiquen por dentro de la clase. 
+        private set
+        {
+            _vida = Mathf.Clamp(value, 0, 100); // evitamos valores invalidos. 
+            if (_vida == 0)
+                Morir();
+        }
+
+    }
+    public float Velocidad
+    {
+        get => _velocidad;
+        set
+        {
+            if (value >= 0)
+                _velocidad = value;
+        }
+    }
+    public int TotalMonedas
+    {
+        get => _totalMonedas; // ver que tengo
+        private set // con set puedo cambiar la variable solo dentro de esta clase.
+        {
+            if (value >= 0)
+                _totalMonedas = value;
+        }
+    }
+    public Vector2 PosicionActual
+    {
+        get => _posicionActual;
+        private set => _posicionActual = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Item> Inventario { get; private set; } = new List<Item>();
+    public List<Ability> HabilidadesDesbloqueadas { get; private set; } = new List<Ability>();
+
+    public bool Vivo // propiedad para solo lectura
     {
-        
+        get { return _vivo; }
+    }
+
+    private void Update()
+    {
+        Vector2 direccion = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Mover(direccion);
+    }
+
+    public void Mover(Vector2 direccion)
+    {
+        if (!_vivo) return;
+        transform.Translate(direccion * _velocidad * Time.deltaTime);
+        PosicionActual = new Vector2(transform.position.x, transform.position.y);
+    }
+    public void RecibirDaño(int cantidad) //cumplo con la interface
+    {
+        if (!_vivo) 
+            return;
+        Vida -= cantidad;
+    }
+    public void Morir()
+    {
+        _vivo = false;
+        // recordar implmentar que se reinicie escena o algo
+    }
+    public void Atacar()
+    {
+
+    }
+    public void AgarrarMoneda()
+    {
+        TotalMonedas++;
+       //sumo moneda. Dps ver si una moneda vale mas o algo. 
+    }
+    public void AgarrarItem(Item item)
+    {
+        if (!Inventario.Contains(item))
+        {
+            Inventario.Add(item);
+            item.Usar(this); 
+        }
+    }
+    public void UsarItem(Item item)
+    {
+        if (Inventario.Contains(item))
+        {
+            item.Usar(this);
+            Inventario.Remove(item);
+        }
     }
 }
