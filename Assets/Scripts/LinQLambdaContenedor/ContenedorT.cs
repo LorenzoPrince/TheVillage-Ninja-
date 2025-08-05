5f2363f93fe2d8ceb908883a60ad7b0887980eb6
LorenzoPrince <lorenzoprince222@gmail.com>
2025-08-05 01:19:42 -0300

    Implementa clase genérica contenedor<T>
    Se agregó una clase genérica Contenedor<T> que permite agregar, remover, obtener, filtrar y ordenar elementos de cualquier tipo T.
    Esta clase facilitará la gestión de colecciones por ejemplo para monedas o futuros objetos.

---
+++ Assets/Scripts/LinQLambdaContenedor/ContenedorT.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Contenedor<T> //Clase generica para manejar listas de cualquier tipo t
+                            //la vamos a usar para coin dps
+{
+    private List<T> _elementos = new List<T>();   // Lista interna que almacena elementos de tipo T
+
+    public void Agregar(T elemento) => _elementos.Add(elemento); //Agregar elementos 
+
+    public void Remover(T elemento) => _elementos.Remove(elemento); //remover elementos
+
+    public List<T> ObtenerTodos() => _elementos; //da todos los elementos de una lista
+
+    public List<T> Filtrar(System.Func<T, bool> predicado) => _elementos.Where(predicado).ToList(); //filtra elemento segun algo y devuelve bool
+
+    public List<T> OrdenarPor(System.Func<T, float> criterio) => _elementos.OrderBy(criterio).ToList(); //ordena segun criteio devuelve float
+}
